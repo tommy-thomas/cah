1cf954455c2d42bf4131a88a3191c0af90935b65
tommy-thomas <tommyt67@gmail.com>
2025-10-14 21:41:06 -0500

    adding python anywhere to allowed hosts

--- cah/settings.py
+++ cah/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-l77@s6dm8%#c@q+9nuz0y34q%z8gft0!oqdlv@$0sj6*53$663
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cah-ishwu.pythonanywhere.com']
 
 
 # Application definition
